439f8aee1f09068dacfbe89f27faffa8ad88cea9
BYRONPM91 <58235612+BYRONPM91@users.noreply.github.com>
2022-07-12 14:41:04 -0500

    Interfaces y métodos

---
+++ Sys_Inventarios/Model/Repository.cs
@@ -0,0 +1,18 @@
+﻿using EFRepository;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Repository : EFRepository.Repository, IRepository, IDisposable
+    {
+        public Repository(bool autoDetectChangesEnabled = false, bool proxiCreationEnabled = false) :
+            base(new Entidades(), autoDetectChangesEnabled, proxiCreationEnabled)
+        {
+        }
+    }
+}
---
+++ Sys_Inventarios/Repository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public interface IRepository : IDisposable
+    {
+        TEntity Create<TEntity>(TEntity createEntity) where TEntity : class;   //Para crear un registro
+        bool Update<TEntity>(TEntity updateEntity) where TEntity : class;   //Para actualizar un registro
+        bool Delete<TEntity>(TEntity deleteEntity) where TEntity : class;   //Para eliminar un registro
+        TEntity FindEntity<TEntity>(Expression<Func<TEntity, bool>> criteria) where TEntity : class;    //Para buscar un registro
+        IEnumerable<TEntity> FindEntitySet<TEntity>(Expression<Func<TEntity, bool>> criteria) where TEntity : class;    //Para buscar varios registros
+        IPagedList<TEntity> FindEntitySetPage<TEntity>(Expression<Func<TEntity, bool>> criteria, Expression<Func<TEntity, string>> order, int page, int pageSize) where TEntity : class;    //Para buscar varios registros
+    }
+}
